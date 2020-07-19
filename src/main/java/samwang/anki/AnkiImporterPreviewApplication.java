952882cb6fae3434cc965a6ab30eafff3a0159f4
sevenbamboos <sevenbamboos@gmail.com>
2020-07-19 20:34:31 +1200

    enable port 3000

--- src/main/java/samwang/anki/AnkiImporterPreviewApplication.java
+++ src/main/java/samwang/anki/AnkiImporterPreviewApplication.java
@@ -19,7 +19,10 @@ public class AnkiImporterPreviewApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins("http://localhost:8081")
+                    .allowedOrigins("http://localhost:3000");
             }
         };
     }
